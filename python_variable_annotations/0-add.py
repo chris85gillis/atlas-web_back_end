4ec685bfc15263dc606e765225df4e1f1868f7eb
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 13:38:13 -0500

    figuring out formating for documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
+"""Add two floats and return the result."""
 
 
 def add(a: float, b: float) -> float:
-    """Add two floats and return the result.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of a and b.
-    """
     return a + b
