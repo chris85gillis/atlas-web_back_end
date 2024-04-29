2caa0368633b0be5a58abebef9e42433d95ad237
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 14:55:20 -0500

    adding task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Convert a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation."""
+    return str(n)
