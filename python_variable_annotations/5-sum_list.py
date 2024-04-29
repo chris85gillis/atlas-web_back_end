1e25a0b0609380f1479b9adee2d2b137b70c86ee
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 15:08:33 -0500

    adding task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Calculate the sum of floats in a list."""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Calculate the sum of floats in a list."""
+    return sum(input_list)
