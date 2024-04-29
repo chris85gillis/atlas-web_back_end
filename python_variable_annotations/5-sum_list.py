83598e9776c61e201ca7b326a3401eb6ba89b765
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 15:09:16 -0500

    reformating my blank lines for 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Calculate the sum of floats in a list."""
-
-
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     """Calculate the sum of floats in a list."""
