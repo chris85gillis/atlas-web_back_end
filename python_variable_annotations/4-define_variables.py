094cf4c7f425ffb9c96b5513736c7e9cc3495697
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 15:04:47 -0500

    fixing documentation error for 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-# Define and annotate the variables
+"""Define and annotate the variables"""
 
 
-# Define and annotate the variables
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
