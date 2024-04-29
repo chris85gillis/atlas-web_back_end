fc6b11c1fe621cfcb22f0da3e13a18e3444e7c28
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-04-29 13:39:15 -0500

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Add two floats and return the result."""
+'''function add takes float a and float b and returns their sum float.'''
 
 
 def add(a: float, b: float) -> float:
