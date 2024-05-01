86abd2e0ccc8f474c5ef30629d86815da219e47a
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-01 17:47:17 -0500

    making some edits to make run correctly

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,7 +3,7 @@
 and returns total_time / n."""
 import time
 from typing import List
-from asyncio import run
+import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
@@ -25,8 +25,11 @@ async def measure_time(n: int, max_delay: int) -> float:
     total_time = time.time() - start_time
     return total_time / n
 
+def measure(n: int, max_delay: int) -> float:
+    return asyncio.run(measure_time(n, max_delay))
+
 if __name__ == "__main__":
     n = 5
     max_delay = 9
-    print(run(measure_time(n, max_delay)))
+    print("Approximate elapsed time falls within an acceptable range: {}.".format(0 <= measure(n, max_delay) <= max_delay * 1.02))
     
