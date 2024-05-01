0b1fae25ca4687d25a76c49668a1363883138e00
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-01 17:51:04 -0500

    got file to work correctly puttin in last edits to file

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,13 +2,12 @@
 """ Measures the total execution time for wait_n(n, max_delay),
 and returns total_time / n."""
 import time
-from typing import List
 import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay),
     and returns total_time / n.
@@ -20,16 +19,8 @@ async def measure_time(n: int, max_delay: int) -> float:
     Returns:
         float: The average execution time per call.
     """
-    start_time = time.time()
-    await wait_n(n, max_delay)
-    total_time = time.time() - start_time
-    return total_time / n
-
-def measure(n: int, max_delay: int) -> float:
-    return asyncio.run(measure_time(n, max_delay))
-
-if __name__ == "__main__":
-    n = 5
-    max_delay = 9
-    print("Approximate elapsed time falls within an acceptable range: {}.".format(0 <= measure(n, max_delay) <= max_delay * 1.02))
-    
+    startTime = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    endTime = time.time()
+    total_time = endTime - startTime
+    return (total_time / n)
