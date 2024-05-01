200c4a9352cb6f789936bd0fee9a66fbd9ce9914
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-01 18:04:15 -0500

    fixing typoo

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,7 +2,7 @@
 """Creates an asyncio.Task for the wait_random coroutine."""
 import asyncio
 
-wait_random = __import__('0-basic_asyn_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
