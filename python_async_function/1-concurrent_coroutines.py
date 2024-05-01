909492aae2dadd4970e3a9a18312175a653c2152
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-01 17:33:11 -0500

    fixed the import for wait_random to be pulled into file

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,8 +2,9 @@
 """Asynchronous coroutine that spawns wait_random n times with the specified max_delay."""
 import asyncio
 from typing import List
-from basic_async_syntax.py import wait_random
 
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
