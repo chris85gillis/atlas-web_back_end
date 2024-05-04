b1287863e99a0b0dcec99586dbb4ada3064b9f16
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-04 12:49:03 -0500

    adding task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Collects 10 random numbers using an async comprehension over async_generator."""
+import asyncio
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Returns:
+        List[float]: List of 10 random numbers.
+    """
+    return [random_number async for random_number in async_generator()]
