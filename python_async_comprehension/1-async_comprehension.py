9e49a7b9a6fdf14cc1659212edb2f5166e0163f3
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-04 12:50:25 -0500

    comment line too long

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Collects 10 random numbers using an async comprehension over async_generator."""
+""" Collects 10 random numbers using an async
+comprehension over async_generator."""
 import asyncio
 from typing import List
 async_generator = __import__("0-async_generator").async_generator
