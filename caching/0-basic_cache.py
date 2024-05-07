46742db0386c1ce46d84896a43f838661a5ef664
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-07 14:05:02 -0500

    adding files for task 0 and readme

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Basic caching module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Defines a basic caching system
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
