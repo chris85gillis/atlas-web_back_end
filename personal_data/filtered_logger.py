a637908378edb9f62b3bcbc16c65c6422fcfd14a
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-14 14:58:14 -0500

    adding task 2 to file

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -38,3 +38,25 @@ class RedactingFormatter(logging.Formatter):
                                    message, self.SEPARATOR)
         record.msg = message
         return super().format(record)
+
+
+PII_FIELDS = (
+    "name",
+    "email",
+    "phone",
+    "ssn",
+	"password"
+)
+DATA_FILE = "user_data.csv"
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logger object for user_data"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
