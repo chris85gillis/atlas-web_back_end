865f07a0c0adb08450eedab44cecec2f2be72bc6
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-14 13:49:35 -0500

    adding documentation to task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Filtered Logger"""
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Using re.sub to replace field values with redaction"""
     return re.sub(r'(?<=\b|\s|^)(' + '|'.join(fields) + r')=[^;]*', r'\1=' + redaction, message)
