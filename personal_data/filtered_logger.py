b307f80db8cf48e96a910a8ea6f7556a37550c62
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-14 14:36:19 -0500

    adding task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Filtered Logger"""
 import re
+import logging
 from typing import List
 
 
@@ -14,3 +15,23 @@ def filter_datum(
     pattern = '|'.join(re.escape(field) for field in fields)
     return re.sub(f'({pattern})=(.*?){re.escape(separator)}',
                   f'\\1={redaction}{separator}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes a RedactingFormatter object."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """A description of the entire function, its parameters, and its return types."""
+        message = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return self._fmt % record.__dict__ + message
