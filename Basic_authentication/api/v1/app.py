517e6718801b648802e1f382a07a0cb568eba686
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 17:59:35 -0500

    reworking auth type and moving before request to the top

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -11,21 +11,30 @@ from api.v1.auth.auth import Auth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 
-auth_type = getenv("AUTH_TYPE")
-if auth_type:
-    if auth_type == "your_auth_type_1":
-        auth = YourAuthType1()
-    elif auth_type == "your_auth_type_2":
-        auth = YourAuthType2()
 
-
-if auth:
-    from api.v1.auth.auth import Auth
+if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
+
+@app.before_request
+def before_request_handler():
+    """Filtering each request"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if request.path not in excluded_paths:
+        if not auth.require_auth(request.path):
+            return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+        
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -48,23 +57,6 @@ def forbidden(error) -> str:
     Error handler for forbidden access with a 403 status code.
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request_handler():
-    """Filtering each request"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        if not auth.require_auth(request.path):
-            return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    return jsonify({"status": "OK"})
 
 
 if __name__ == "__main__":
