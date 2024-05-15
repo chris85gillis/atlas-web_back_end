ac741f1da34762b38b750966eef0ce24b7fcecd8
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 17:49:37 -0500

    updates to app

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,49 +21,41 @@ if auth_type:
     elif auth_type == "your_auth_type_2":
         auth = YourAuthType2()
 
-
 if auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """
-    Error handler for unauthorized access with a 401 status code.
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """
-    Error handler for forbidden access with a 403 status code.
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
 @app.before_request
 def before_request_handler():
-    """filtering the request to be authorized"""
+    """Filtering each request"""
     if auth is None:
         return
-    if request.path not in ['/api/v1/status/', '/api/v1/unauthorized/',
-                            '/api/v1/forbidden/']:
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path not in excluded_paths:
         if not auth.require_auth(request.path):
             return
+
     if auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Not found handler """
+    return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized(error):
+    """ Error handler for unauthorized access with a 401 status code. """
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error):
+    """ Error handler for forbidden access with a 403 status code. """
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
