e44b28b3731daef054f49f43ffb77f9a1bc068fd
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 18:15:32 -0500

    update

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,6 +25,23 @@ if auth:
     auth = Auth()
 
 
+@app.before_request
+def authentication_handler() -> None:
+    """Auth handler
+
+    Returns:
+        [type]: creates an instance of auth
+    """
+    exceptions = ['/api/v1/status/',
+                  '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth or not auth.require_auth(request.path, exceptions):
+        return None
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
+        
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -46,22 +63,6 @@ def forbidden(error) -> str:
     Error handler for forbidden access with a 403 status code.
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request_handler():
-    """Filtering each request"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        if not auth.require_auth(request.path):
-            return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    return jsonify({"status": "OK"}), 200
 
 
 if __name__ == "__main__":
