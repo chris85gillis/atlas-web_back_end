52d6e6016b2e5cb3fd21a315d6ffd75e611fbb6d
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 17:43:10 -0500

    updating app authorization_header

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,17 +8,11 @@ from flask import request
 
 class Auth:
     """public mehtods for authentication"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if auth is required, returning a boolean"""
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        path = path.rstrip('/')
-        for excl_path in excluded_paths:
-            if path == excl_path.rstrip('/'):
-                return False
-        return True
+    def authorization_header(self, request=None) -> str:
+        """Returns the Authorization header value from the request"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def authorization_header(self, request=None) -> str:
         """Returns the value of the Authorization
