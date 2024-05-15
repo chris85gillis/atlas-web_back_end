b5a0ac5e282595914a0c7e1af45b2103a8d5d6f1
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 17:13:35 -0500

    adding in task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,14 @@ class Auth:
     """public mehtods for authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if auth is required, returning a boolean"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excl_path in excluded_paths:
+            if path.rstrip('/') == excl_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the authorization header from a request as a string"""
