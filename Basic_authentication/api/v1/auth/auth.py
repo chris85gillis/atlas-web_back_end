9b306e10bbea37f23997cbba6f0b432baefc9daa
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-15 17:05:33 -0500

    adding documentation to 3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,13 @@ from flask import request
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ public method """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ public method """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ public method """
         return None
