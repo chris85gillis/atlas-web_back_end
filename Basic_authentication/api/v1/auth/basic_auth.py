ad71bc5b6a5d2a2cca079069b2bb21b79e7a0d7c
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:48:00 -0500

    adding task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 class for basic authentications
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
             return None
         return authorization_header.split(" ")[1] \
             if len(authorization_header.split(" ")) == 2 else None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the base64 authorization header and return the decoded string.
+        """
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except:
+            return None
