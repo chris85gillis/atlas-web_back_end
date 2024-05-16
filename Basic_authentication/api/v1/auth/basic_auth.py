f8917df37213a52f8daf338a11eef2c44ecc8621
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:34:33 -0500

    adding task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,10 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class for BasicAuth"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        # Split the header and extract the base64 part
+        return authorization_header.split(" ")[1] if len(authorization_header.split(" ")) == 2 else None
