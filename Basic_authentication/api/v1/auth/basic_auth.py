abdaa41c96f670ce8cd4089d98a1173486213048
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:57:25 -0500

    adding task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,16 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts user credentials from decoded base64 authorization header.
+        """
+        if decoded_base64_authorization_header is None or \
+                not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password
