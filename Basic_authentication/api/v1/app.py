0356c55d05c9702e7c813148900d85760cb65c1c
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:19:51 -0500

    trying a different way, importing os for getenv

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,18 +6,19 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv('AUTH_TYPE') == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth as Auth
-else:
+if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
-    if getenv('AUTH_TYPE') == 'auth':
-        auth = Auth()
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 @app.before_request
 def before_request() -> str:
