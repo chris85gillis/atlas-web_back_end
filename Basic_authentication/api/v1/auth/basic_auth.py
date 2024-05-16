05ccbc51345e045eeee363940b1430c3fd8f44ad
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:41:16 -0500

    reworking line length

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,15 +1,21 @@
-# api/v1/auth/basic_auth.py
-
+#!/usr/bin/env python3
+"""
+class for basic authentications
+"""
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+    """class for BasicAuth"""
+    def extract_base64_authorization_header(self,
+                                             authorization_header: str) -> str:
+        """
+        A function to extract the base64 part from the authorization header.
+        """
         if authorization_header is None or \
                 not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        # Split the header and extract the base64 part
         return authorization_header.split(" ")[1] \
             if len(authorization_header.split(" ")) == 2 else None
