d896b48a143bc514ee948fda0538a056699f631f
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:35:35 -0500

    adding extra documentation

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,9 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """class for BasicAuth"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        A function to extract the base64 part from the authorization header.
+        """
         if authorization_header is None or not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        # Split the header and extract the base64 part
         return authorization_header.split(" ")[1] if len(authorization_header.split(" ")) == 2 else None
