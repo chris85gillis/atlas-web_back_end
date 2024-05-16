6b5c3b3b6517b689fbf004ae550b8fa4d7463688
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-16 15:14:39 -0500

    import errors for basicauth trying to resolve

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,21 +7,17 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv('AUTH_TYPE') == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-
 if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth as Auth
 else:
     from api.v1.auth.auth import Auth
-    
+    if getenv('AUTH_TYPE') == 'auth':
+        auth = Auth()
 
 @app.before_request
 def before_request() -> str:
