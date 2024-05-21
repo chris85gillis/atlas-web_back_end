760f5bbb53c561217ed68a42aae8ad708edea05b
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-21 15:23:04 -0500

    adding task 7

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+from api.v1.app import auth
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Logs in a user using session authentication."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user[0].id)
+
+    response_data = {
+        "email": user[0].email,
+        "first_name": user[0].first_name,
+        "last_name": user[0].last_name
+    }
+
+    response = jsonify(response_data)
+    response.set_cookie(key=getenv("SESSION_NAME", "_my_session_id"), value=session_id)
+    return response
