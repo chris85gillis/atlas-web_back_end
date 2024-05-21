e0ba82b5e47c9e3438d178d7bc93631f53a3d062
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-21 15:44:24 -0500

    reworking login function to handle errors better

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -11,23 +11,21 @@ import os
 def login() -> str:
     """Logs in a user using session authentication."""
     email = request.form.get('email')
-    password = request.form.get('password')
-
     if not email:
         return jsonify({"error": "email missing"}), 400
-    if not password:
+    pwd = request.form.get('password')
+    if not pwd:
         return jsonify({"error": "password missing"}), 400
-
-    user = User.search({'email': email})
-    if not user:
+    user_list = User.search({'email': email})
+    if not user_list:
         return jsonify({"error": "no user found for this email"}), 404
-
-    if not user[0].is_valid_password(password):
+    user = user_list[0]
+    if not user.is_valid_password(pwd):
         return jsonify({"error": "wrong password"}), 401
 
     session_id = auth.create_session(user.id)
 
     response = make_response(jsonify(user.to_json()))
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
-    
+
     return response
