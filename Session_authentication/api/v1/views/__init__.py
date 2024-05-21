ea1475cb294a199e0ec5c8a4defba044b48f09d3
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-21 15:31:14 -0500

    adding new view to init file

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
+from api.v1.views.users import User, session_auth
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
@@ -9,3 +10,5 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 
 User.load_from_file()
+
+app_views.register_blueprint(session_auth.app_views)
