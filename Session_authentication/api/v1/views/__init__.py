9697e1575f05e8f3baf94b236b4e2e205c591808
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-21 15:37:36 -0500

    adding documentation

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -4,11 +4,14 @@
 from flask import Blueprint
 from api.v1.views.users import User, session_auth
 
+# Create a Blueprint instance for the views
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
 
+# Load users from file
 User.load_from_file()
 
+# Register the session_auth blueprint
 app_views.register_blueprint(session_auth.app_views)
