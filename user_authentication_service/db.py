9cbbf0538a92b0ac4946878ad8d5af82a6fe9043
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-24 14:58:17 -0500

    adding task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base, User
 
 
@@ -36,3 +36,13 @@ class DB:
         self._session.add(user_to_add)
         self._session.commit()
         return user_to_add
+
+    def find_user_by(self, **kwargs) -> User:
+        '''Returns the first row found in the users'''
+        try:
+            find_usr = self._session.query(User).filter_by(**kwargs).first()
+            if find_usr is None:
+                raise NoResultFound
+            return find_usr
+        except TypeError:
+            raise InvalidRequestError
