4b2cf6bda52a89d4dcd4ce1cb31ab3aeeaf7e1e7
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-24 15:25:14 -0500

    adding task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,7 +3,26 @@
 Module handling password hashing using the bycrypt library.
 """
 import bcrypt
+from db import DB
+from user import User
+from auth import _hash_password
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user."""
+        existing_user = self._db.find_user_by(email=email)
+        if existing_user:
+            raise ValueError(f"User {email} already exists")
+        
+        hashed_password = _hash_password(password)
+        new_user = self._db.add_user(email, hashed_password)
+        return new_user
 
 def _hash_password(password: str) -> bytes:
     '''Hashes password'''
