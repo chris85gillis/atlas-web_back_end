b2d2d7c7e58b6754b19297d1a7e53bcedf63b763
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-25 15:29:09 -0500

    update to 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -82,10 +82,8 @@ class Auth:
         """Generate a reset password token for a user."""
         try:
             user = self._db.find_user_by(email=email)
-            if user is None:
-                raise ValueError(f'User {email} does not exist')
-            token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=token)
-            return token
         except NoResultFound:
-            raise ValueError(f'User {email} does not exist')
+            raise ValueError("User does not exist")
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
