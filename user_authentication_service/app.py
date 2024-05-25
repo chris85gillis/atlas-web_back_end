b0dfa5ec38e4077b7f6ba51cd61018171a65ec30
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-25 16:22:03 -0500

    adding task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -86,5 +86,21 @@ def reset_password() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+	'''Get update password func'''
+	email = request.form.get('email')
+	reset_token = request.form.get('reset_token')
+	new_password = request.form.get('new_password')
+	if not email or not reset_token or not new_password:
+		abort(403)
+	try:
+		AUTH.update_password(reset_token, new_password)
+		return jsonify({"email": f"{email}", "message":
+						"Password updated"}), 200
+	except ValueError:
+		abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
