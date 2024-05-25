06dd71ed371bb04edda5d0dc735e3016e8bacc29
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-25 15:03:01 -0500

    adding task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -41,6 +41,21 @@ def login():
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie('session_id', session_id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """
+    Endpoint to log out a user.
+    """
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
