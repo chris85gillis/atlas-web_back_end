336daffb1e2a107f8af55f648b1a206d317ce781
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-25 15:09:00 -0500

    adding task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -77,3 +77,15 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Get a reset password token for the user with the given email.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError("User does not exist")
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
