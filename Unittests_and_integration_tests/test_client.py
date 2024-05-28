8bb7eef15cb640a1046488512f317e7a064f9d43
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-05-28 17:30:21 -0500

    testing

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
-from utils import get_json, access_nested_map, memoize
 
 class TestGithubOrgClient(unittest.TestCase):
     """
@@ -13,14 +12,18 @@ class TestGithubOrgClient(unittest.TestCase):
         ('google',),
         ('abc',),
     ])
-    @patch('client.get_json')
+    @patch('client.GithubOrgClient.get_json')
     def test_org(self, org_name, mock_get_json):
-        '''Test org'''
+        """
+        Test that GithubOrgClient.org returns the correct value.
+        """
+        expected_response = {'name': org_name}
+        mock_get_json.return_value = expected_response
         client = GithubOrgClient(org_name)
-        client.org()
+        result = client.org
+        self.assertEqual(result, expected_response)
         mock_get_json.assert_called_once_with
         (GithubOrgClient.ORG_URL.format(org=org_name))
-
 
 if __name__ == "__main__":
     unittest.main()
