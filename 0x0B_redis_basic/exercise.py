40c56802d1465727ca8321f08153abc9f789150c
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-09 18:34:03 -0500

    adding task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -22,3 +22,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, str, int, float]:
+        """
+        Get data from Redis
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return data
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """
+        Get data from Redis as string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Get data from Redis as integer
+        """
+        return self.get(key, fn=int)
