9928841638c603fc7f76df52d1f11654f277414c
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 13:41:40 -0500

    update to task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,11 +1,7 @@
 -- Trigger to decrease the quantity
 
 
-CREATE TRIGGER decrease_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET items.quantity = items.quantity - NEW.number
-    WHERE items.name = NEW.item_name;
-END
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+    FOR EACH ROW UPDATE items
+        SET items.quantity = items.quantity - NEW.number
+        WHERE items.name = NEW.item_name;
