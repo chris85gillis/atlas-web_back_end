dd2d0b635430f353d1dfa39f341ff324646ca3da
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 12:40:40 -0500

    adding comments to top of task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,11 @@
+-- This script creates a 'users' table with the specified attributes.
+-- The 'users' table includes the following columns:
+-- - id: an integer that is never null, auto increments, is the primary key
+-- - email: a string (255 characters), never null, and unique
+-- - name: a string (255 characters)
+-- If the table already exists, the script will not fail.
+
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
