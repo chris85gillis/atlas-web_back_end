f956d0f5829c79c9211fb5e685e6f06532e14be6
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 14:47:28 -0500

    reworking task 7

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,15 @@
--- This script adds a procedure to add a bonus to a project that a user has contributed to.
--- The procedure takes three parameters: user_id (the id of the user who contributed), project_name (the name of the project), and score (the bonus score).
--- The procedure first checks if the project exists. If it does not, it creates a new project with the given name.
--- Then, it retrieves the project's id using the project's name.
--- Finally, it inserts a new correction record into the corrections table with the user_id, project_id, and score parameters.
+-- ComputeAverageScoreForUser: procedure to compute the average score for a user.
 
 
-DELIMITER $$
-DROP PROCEDURE IF EXISTS AddBonus;
-CREATE PROCEDURE AddBonus (
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-)
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE projects.name = project_name) = 0 THEN
-        INSERT INTO projects (name) VALUES (project_name);
-    END IF;
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    DECLARE avg_score FLOAT;
 
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END $$
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
+
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END;
+//
+
 DELIMITER ;
