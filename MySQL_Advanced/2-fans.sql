09ad985975b7175cc6597fe89a3d1c19bdde81d9
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 13:08:45 -0500

    update to task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,14 +1,15 @@
--- This SQL script shows the number of fans per country_origin for each metal band.
--- It uses a subquery to count the number of fans per band, and then groups by country_origin.
--- The result is ordered by the number of fans in descending order.
+-- This script ranks country origins of bands, ordered by the number of (non-unique) fans
 
+-- Ensure the database is selected
+USE metal_bands.sql;
 
-SELECT
-    country_origin AS origin,
-    COUNT(DISTINCT(id_fan)) AS nb_fans
-FROM
-    metal_bands
-GROUP BY
-    country_origin
-ORDER BY
+-- Select the origin and sum of fans, grouping by origin and ordering by the sum of fans in descending order
+SELECT 
+    origin, 
+    SUM(fans) AS nb_fans 
+FROM 
+    metal_bands 
+GROUP BY 
+    origin 
+ORDER BY 
     nb_fans DESC;
