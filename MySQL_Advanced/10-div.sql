8ea374fb717bd7629ed79c0aa66f4be6d837f570
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 15:05:23 -0500

    adding task 10

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- A safe division function that returns 0 if the second argument is 0
+
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+DELIMITER ;
