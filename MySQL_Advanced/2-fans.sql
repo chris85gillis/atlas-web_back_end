af99fc4705974859bf58cee296703545b19caa0b
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 13:23:11 -0500

    edits to task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,15 +1,6 @@
 -- This script ranks country origins of bands, ordered by the number of (non-unique) fans
-
--- Ensure the database is selected
-USE metal_bands.sql;
-
 -- Select the origin and sum of fans, grouping by origin and ordering by the sum of fans in descending order
-SELECT 
-    origin AS origin,
-    SUM(fans) AS nb_fans 
-FROM 
-    metal_bands 
-GROUP BY 
-    origin 
-ORDER BY 
-    nb_fans DESC;
+
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
