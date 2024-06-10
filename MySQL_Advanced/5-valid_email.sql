f66df7db468b5dede714fd43acfa0ef45410d100
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 14:01:18 -0500

    update to task 5

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,13 @@
--- Create trigger to reset valid_email only when email is updated
+-- This trigger resets the 'valid_email' field to 0 when the 'email' field is updated.
+-- This is useful when we want to reset the validation status of an email address.
+-- For example, if a user resets their password, we want to reset the 'valid_email' field.
 
+
+DELIMITER $$
 CREATE TRIGGER reset_valid_email_trigger BEFORE UPDATE ON users
     FOR EACH ROW
         IF OLD.email != NEW.email THEN
             SET NEW.valid_email = 0;
         END IF;
+    END $$
+DELIMITER ;
