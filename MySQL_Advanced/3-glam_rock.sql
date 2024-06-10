a05e4845b6f7219e310720ee87d6afb6cc99600d
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-10 13:34:18 -0500

    adding task 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,8 @@
--- This SQL query selects the band name and calculates the lifespan of glam metal bands in the metal_bands table.
--- The lifespan is calculated by subtracting the release date from the farewell date and displaying the result as a year.
--- The result is ordered by lifespan in descending order.
+
+-- this script will calculate the lifespan of metal bands that have a specific style
+-- and sort them in descending order.
+-- the lifespan is the number of years a band has been active.
+-- the IFNULL function is used to get the current year if the band is still active.
 
 
 SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
