3c28d4f09bb678269dee3b86ee17254e957cd06f
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-06-12 15:13:20 -0500

    adding task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["logs"]
+collection = db["nginx"]
+
+count = collection.count_documents({})
+
+get_count = collection.count_documents({"method": "GET"})
+post_count = collection.count_documents({"method": "POST"})
+put_count = collection.count_documents({"method": "PUT"})
+patch_count = collection.count_documents({"method": "PATCH"})
+delete_count = collection.count_documents({"method": "DELETE"})
+
+status_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+print(str(count) + " logs")
+print("Methods:")
+print("\tmethod GET: " + str(get_count))
+print("\tmethod POST: " + str(post_count))
+print("\tmethod PUT: " + str(put_count))
+print("\tmethod PATCH: " + str(patch_count))
+print("\tmethod DELETE: " + str(delete_count))
+print(str(status_count) + " status check")
