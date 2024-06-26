52ffa57459272ee090016c628f977d06a785fea9
Chris Gillis <chris85gillis@gmail.com>
2024-06-25 21:42:40 -0500

    adding task 9

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,18 @@
+// This file contains a module that sends a payment request to the API
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Welcome to the payment system');
+});
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
