739a7791f6197b3cb5425e76851b4584cf0bd38f
Chris Gillis <chris85gillis@gmail.com>
2024-06-26 20:09:01 -0500

    adding file 8-job

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,28 @@
+export const createPushNotificationsJobs = (jobs, queue) => {
+    if (!Array.isArray(jobs)) {
+      throw new Error('Jobs is not an array');
+    }
+  
+    jobs.forEach((jobData) => {
+      const job = queue.create('push_notification_code_3', jobData).save((err) => {
+        if (err) {
+          console.log(`Error creating job: ${err}`);
+        } else {
+          console.log(`Notification job created: ${job.id}`);
+        }
+      });
+  
+      job.on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      });
+  
+      job.on('failed', (errorMessage) => {
+        console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+      });
+  
+      job.on('progress', (percentage) => {
+        console.log(`Notification job ${job.id} ${percentage}% complete`);
+      });
+    });
+  };
+  
